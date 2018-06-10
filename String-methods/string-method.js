dd3351e79b71ace90424e8df6ea9dd82a139d01b
Sam Cherian <samnp87@gmail.com>
2018-06-10 16:05:57 +0530

    Commented in JS file-to know how this result was acheived

--- String-methods/string-method.js
+++ String-methods/string-method.js
@@ -3,12 +3,16 @@ var cities = ['lonDon', 'ManCHESTer', 'BiRmiNGHAM', 'liVERpoOL'];
 
 for(var i = 0; i < cities.length; i++) {
 var input = cities[i];
+// Change all the array items to lowercase-method used toLowerCase()
 var lowerCase = input.toLowerCase();
+// then the find the first letter to UpperCase -method used toUpperCase() and slice() 
 var firstLetter = lowerCase.slice(0,1).toUpperCase();
+// replace method is used to replace the first letter from
+// text that was changed to lowercase is applied the replace() method
 var newWord = lowerCase.replace(lowerCase[0],firstLetter);
-  var result = newWord;
-  console.log(result);
+var result = newWord;
+ 
 var listItem = document.createElement('li');
-  listItem.textContent = result;
- list.appendChild(listItem);
+listItem.textContent = result;
+list.appendChild(listItem);
 }
